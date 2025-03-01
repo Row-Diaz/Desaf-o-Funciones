d03df643c8dcca03ab09514a20882c59f9671b82
ROW <axlrower@gmail.com>
2025-03-01 20:46:18 -0300

    resolucion del segundo ejercicio

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -1,12 +1,14 @@
 // EJERCICIO N° 1
 
 // Declaracion de funcion
-function example(a,b,c){
-    return a+b+c
+function example(a, b, c) {
+  return a + b + c;
 }
 
 // Funcion de expresión
 
-let example = function(a, b ,c){
-    return a+b+c;
-}
+let example = function (a, b, c) {
+  return a + b + c;
+};
+
+
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,11 @@
+// EJERCICIO N° 2
+
+// Funcion inicial
+
+var suma = function(a,b) {
+    return a + b;
+}
+
+//Conversion para arrow function
+
+var suma = (a, b) => a + b;
