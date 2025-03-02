24662524827a64d5aa4cedac77966489229f9e10
ROW <axlrower@gmail.com>
2025-03-01 22:53:23 -0300

    resolucion de los apartados 2.2 y 2.3

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+ 
+ // Por favor descomentar los codigos por separado para ver el funcionamiento de cada uno en el archivo HTML
+ 
+ 
+ /* Resolucion del apartado 2.2 */
+ 
+  const ele = document.getElementById("ele1")
+  ele.addEventListener("click", function() {
+      ele.style.backgroundColor = 'yellow'
+  });
+    
+
+
+/*Resolucion del apartado 2.3*/
+
+    // ele = document.getElementById("ele1");
+    // ele.style.backgroundColor = "green";
+    // ele.addEventListener("click" ,() => ele.style.backgroundColor = "yellow")
