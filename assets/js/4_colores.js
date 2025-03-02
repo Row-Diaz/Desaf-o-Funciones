c5accbcafd60b82f04ffab8601b570b47821248d
ROW <axlrower@gmail.com>
2025-03-02 00:24:29 -0300

    4 colores culminado

---
+++ assets/js/4_colores.js
@@ -0,0 +1,10 @@
+let colAzul = document.getElementById("color1");
+let colRojo = document.getElementById ("color2");
+let colVerde = document.getElementById ("color3");
+let colAmarillo = document.getElementById ("color4");
+
+colAzul.addEventListener('click', () => colAzul.style.backgroundColor = 'black');
+colRojo.addEventListener('click', () => colRojo.style.backgroundColor = 'black');
+colVerde.addEventListener('click', () => colVerde.style.backgroundColor = 'black');
+colAmarillo.addEventListener('click', () => colAmarillo.style.backgroundColor = 'black');
+
