0ddb2aa8f0c872aae3e36ea6888973ac40ac77b1
ROW <axlrower@gmail.com>
2025-03-02 14:46:07 -0300

    culminacion ejercicio de las letras a,s,d/q,w,e

---
+++ assets/js/letras_a_s_d.js
@@ -0,0 +1,28 @@
+let caja1 = document.getElementById('key')
+let caja2 = document.getElementById('key2')
+
+document.addEventListener('keydown', function (event) {
+    
+    if (event.key === 'a') {
+        caja1.style.backgroundColor= 'pink'
+    } 
+    else if (event.key === 's') {
+        caja1.style.backgroundColor= 'orange'
+    }
+    else if (event.key === 'd') {
+        caja1.style.backgroundColor= 'skyblue'
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    caja2.classList.remove('inactive');
+    if (event.key === 'q') {
+        caja2.style.backgroundColor= 'purple'
+    } 
+    else if (event.key === 'w') {
+        caja2.style.backgroundColor= 'grey'
+    }
+    else if (event.key === 'e') {
+        caja2.style.backgroundColor= 'brown'
+    }
+});
